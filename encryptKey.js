c29a32955a9ed4b5486c442d60e62c6b9677439f
Ritik Sachan <sachanritik1@gmail.com>
2023-04-10 18:43:16 +0530

    first commit

---
+++ encryptKey.js
@@ -0,0 +1,23 @@
+const ethers = require("ethers");
+const fs = require("fs");
+require("dotenv").config();
+
+async function main() {
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+  const encryptedJsonKey = await wallet.encrypt(
+    process.env.PRIVATE_KEY_PASSWORD,
+    process.env.PRIVATE_KEY
+  );
+  console.log(encryptedJsonKey);
+  fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+//after encrytion of private key --->
+// we can remove our private key and password from the .env file
